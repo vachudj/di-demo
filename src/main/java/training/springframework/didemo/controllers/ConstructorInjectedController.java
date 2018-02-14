d538f0a6fb021239fe03a394a056042e35327943
jan.vachuda <jan.vachuda@4finance.com>
2018-02-14 10:55:21 +0100

    3-32 - DI with spring framework.

--- src/main/java/training/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/training/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -1,16 +1,21 @@
 package training.springframework.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import training.springframework.didemo.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
     
     private GreetingService greetingService;
 
+    @Autowired //Not needed here but it's still good practice.
     ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
     
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 }
--- src/main/java/training/springframework/didemo/controllers/SetterInjectedController.java
+++ src/main/java/training/springframework/didemo/controllers/SetterInjectedController.java
@@ -1,15 +1,20 @@
 package training.springframework.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import training.springframework.didemo.services.GreetingService;
 
+@Controller
 public class SetterInjectedController {
     
     private GreetingService greetingService;
     
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
-    
+
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
