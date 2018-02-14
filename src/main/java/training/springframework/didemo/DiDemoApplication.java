47a6db790f6e81d89a5216e47e470b47afbeb572
jan.vachuda <jan.vachuda@4finance.com>
2018-02-14 07:48:06 +0100

    3-29 - The Spring Framework context

--- src/main/java/training/springframework/didemo/DiDemoApplication.java
+++ src/main/java/training/springframework/didemo/DiDemoApplication.java
@@ -2,11 +2,18 @@ package training.springframework.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import training.springframework.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+		
+		controller.hello();
 	}
 }
---
+++ src/main/java/training/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package training.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!!!");
+        return "foo";
+    }
+}
