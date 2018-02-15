ee222867bc6b4c3fc8807a292ff972c1bab63fd3
jan.vachuda <jan.vachuda@4finance.com>
2018-02-15 07:53:36 +0100

    3-34 - Primary annotation introduction.

--- src/main/java/training/springframework/didemo/controllers/MyController.java
+++ src/main/java/training/springframework/didemo/controllers/MyController.java
@@ -2,11 +2,19 @@ package training.springframework.didemo.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import training.springframework.didemo.services.GreetingService;
+
 @Controller
 public class MyController {
+    
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String hello() {
         System.out.println("Hello!!!");
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
