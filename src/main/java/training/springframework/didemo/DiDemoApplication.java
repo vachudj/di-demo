5714568dcbdb673c76fea29f94d6b1f971a8b51e
jan.vachuda <jan.vachuda@4finance.com>
2018-03-09 07:32:21 +0100

    4-72 - Single/Multiple properties.

--- src/main/java/training/springframework/didemo/DiDemoApplication.java
+++ src/main/java/training/springframework/didemo/DiDemoApplication.java
@@ -8,6 +8,8 @@ import training.springframework.didemo.controllers.ConstructorInjectedController
 import training.springframework.didemo.controllers.MyController;
 import training.springframework.didemo.controllers.PropertyInjectionController;
 import training.springframework.didemo.controllers.SetterInjectedController;
+import training.springframework.didemo.examplebeans.FakeDataSource;
+import training.springframework.didemo.examplebeans.FakeJmsBroker;
 
 @SpringBootApplication
 public class DiDemoApplication {
@@ -17,9 +19,21 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+
+		FakeJmsBroker fakeJmsBroker = ctx.getBean(FakeJmsBroker.class); 
+		
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectionController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+		System.out.println(fakeDataSource.getUser());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
+
+		System.out.println(fakeJmsBroker.getUsername());
+		System.out.println(fakeJmsBroker.getPassword());
+		System.out.println(fakeJmsBroker.getUrl());
 	}
 }
---
+++ src/main/java/training/springframework/didemo/config/PropertyConfig.java
@@ -0,0 +1,61 @@
+package training.springframework.didemo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import training.springframework.didemo.examplebeans.FakeDataSource;
+import training.springframework.didemo.examplebeans.FakeJmsBroker;
+
+@Configuration
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+    @PropertySource("classpath:datasource.properties"),
+    @PropertySource("classpath:jms.properties")    
+})
+public class PropertyConfig {
+    
+    @Value("${guru.username}")
+    String user;
+    
+    @Value("${guru.password}")
+    String password;
+    
+    @Value("${guru.dburl}")
+    String url;
+
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jsmUrl;
+    
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUsername);
+        return fakeJmsBroker;
+    }
+    
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
